9b93c2baad13ca814907b762d6194a8e88012229
HvaiY <337646685@qq.com>
2020-11-26 18:03:08 +0800

    博客仓库初始化

--- deploy.sh
+++ deploy.sh
@@ -14,21 +14,36 @@ cd docs/.vuepress/dist
 
 echo "-------Begin Git-------"
 #第一次发布执行
-#git init
 
-git status
+# git init
+# git commit -m "deploy"
+# git branch -M main
+# git remote add origin https://github.com/HvaiY/HvaiY.github.io.git
+# git push -u origin main
+# 第一次提交到存储库之后第二次执行
+cd ../
+git clone https://github.com/HvaiY/HvaiY.github.io.git 
+cd dist
+cp -r ./ ../HvaiY.github.io/
+cd ../HvaiY.github.io
 git add -A
 read -p "提交信息:" commitinfo
 git commit -m $commitinfo
-
-
-git commit -m result
-#第一次发布执行
-#git branch -M main
-#第一次发布执行
-# git remote add origin https://github.com/HvaiY/HvaiY.github.io.git
-# 如果你想要部署到 https://USERNAME.github.io
 git push -u origin main
+cd ../
+rm -rf HvaiY.github.io/
+# git rebase remotes/origin/main
+# git rebase --continue
+#git merge main
+# git remote rm origin 
+# git push -u origin main
+
+# git branch -M main
+# git remote add origin https://github.com/HvaiY/HvaiY.github.io.git
+# git pull
+
+# # # 如果你想要部署到 https://USERNAME.github.io
+# git push -u origin main
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
