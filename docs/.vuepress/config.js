bc3209adacb472e4150a72540def6b003d5b511c
HvaiY <337646685@qq.com>
2020-12-11 11:06:19 +0800

    配置侧边栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,57 @@
 module.exports = {
-    title: 'Hvai\'s blog',
-    description: '我的个人网站,暂支持文章发布',
-    head: [ // 注入到当前页面的 HTML <head> 中的标签
-      ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
-    ], 
-    serviceWorker: true ,// 是否开启 PWA 
-    base: '/', // 这是部署到github相关的配置
-    markdown: {
-      lineNumbers: false // 代码块显示行号
+  title: 'Hvai\'s blog',
+  description: '我的个人网站,暂支持文章发布',
+  head: [ // 注入到当前页面的 HTML <head> 中的标签
+    ['link', {
+      rel: 'icon',
+      href: '/logo.jpg'
+    }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  serviceWorker: true, // 是否开启 PWA 
+  base: '/', // 这是部署到github相关的配置
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    nav: [ // 导航栏配置
+      {
+        text: '前端基础',
+        link: '/accumulate/'
+      },
+      {
+        text: '算法题库',
+        link: '/algorithm/'
+      },
+      {
+        text: '百度',
+        link: 'https://baidu.com'
+      }
+    ],
+    // sidebar: 'auto', // 侧边栏配置
+    sidebar: [{
+      title: 'Home',
+      collapsable: true,
+      path:'/'
+    }, {
+      title: 'MarkDown文档',
+      collapsable: true,
+      // path:'/MD/',
+       children: [{title:'MarkDown使用',path:'./MD/MarkDownDemo',collapsable: false,}]
+    }],
+    // sidebarDepth: 2, // 侧边栏显示2级
+  },
+  markdown: {
+    // markdown-it-anchor 的选项
+    anchor: {
+      permalink: true
     },
-    themeConfig: {
-      nav:[ // 导航栏配置
-        {text: '前端基础', link: '/accumulate/' },
-        {text: '算法题库', link: '/algorithm/'},
-        {text: '百度', link: 'https://baidu.com'}      
-      ],
-      sidebar: 'auto', // 侧边栏配置
-      sidebarDepth: 2, // 侧边栏显示2级
+    // markdown-it-toc 的选项
+    toc: {
+      includeLevel: [1, 2]
+    },
+    extendMarkdown: md => {
+      // 使用更多的 markdown-it 插件!
+      // md.use(require('markdown-it-xxx'))
     }
-  };
+  }
+};
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,27 @@
-####  测试 我的文章 
-##### [搭建gitlab私有库](gitlab.html)
+<!-- [[toc]] -->
+## 测试 我的文章 
+## [Home](/)
+## [CentOS 搭建Gitlab私有代码托管服务](/gitlab)
+## [MarkDown基础语法](/MD/MarkDownDemo.html)
+  - [MarkDown基础语法](/MD/MarkDownDemo)
+
+<!-- --- 
+home: true
+heroImage: /hero.png
+heroText: Hero 标题
+tagline: Hero 副标题
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+- title: 简洁至上
+  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动
+  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+- title: 高性能
+  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+footer: MIT Licensed | Copyright © 2018-present Evan You
+---
+
 
  ``` js
 export default {
@@ -23,4 +45,4 @@ this is a tip
 this is a tip
 :::
 - [emoji](https://www.webfx.com/tools/emoji-cheat-sheet/)
-:tada: :100: :bamboo: :gift_heart: :fire:
+:tada: :100: :bamboo: :gift_heart: :fire: -->
